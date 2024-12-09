41c9a797d0695495f20a1d84aa5b785092a5829d
rajdevweb <wordhello160@gmail.com>
2024-12-09 18:44:01 +0530

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html'],
+  content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+    colors:{
+    "darkgray": "#262626",
+    "white":"#ffffff",
+    "black":"#000000",
+    "hote-cinamon":"#cf6c2a",
+    "dark-hote-cinemon":"#000000",
+    "green-gossamer":"#56b193",
+    "electric-violet":"#ede5ff",
+    "dark-electric-violet":"#5706ac",
+    "cherry-pie":"#c5a7ff"
+    },
+    fontFamily:{
+    "primary-font":["Lato", "sans-serif"],
+    "secondary-font":["Public Sans", "serif"]
+    }
+
+    },
   },
   plugins: [],
 }
